c53d35d5297205688250cf017988613a44594ca5
Viresh Soedhwa <vsoedhwa@bcit.ca>
2026-01-23 09:57:39 -0800

    update gitignore and setup sample scripts

--- etl.sh
+++ etl.sh
@@ -8,21 +8,9 @@ fi
 
 echo "[ETL] $(date -Iseconds) ETL scripts running ..."
 
-RELEASE_DATE="$RELEASE_DATE" # release date for the static files from GTFS translink data
+RELEASE_DATE="$RELEASE_DATE" #
 
 COMMON="/opt/spark/bin/spark-submit --master spark://spark-master:7077"
 
-$COMMON etl-scripts/calendar_etl.py "$RELEASE_DATE" translink-data
-$COMMON etl-scripts/calendar_dates_etl.py "$RELEASE_DATE" translink-data
-$COMMON etl-scripts/routes_etl.py "$RELEASE_DATE" translink-data
-$COMMON etl-scripts/stops_etl.py "$RELEASE_DATE" translink-data
-$COMMON etl-scripts/stop_times_etl.py "$RELEASE_DATE" translink-data
-$COMMON etl-scripts/shapes_etl.py "$RELEASE_DATE" translink-data
-$COMMON etl-scripts/transfers_etl.py "$RELEASE_DATE" translink-data
-$COMMON etl-scripts/trips_etl.py "$RELEASE_DATE" translink-data
+$COMMON etl-scripts/calendar_etl.py "$RELEASE_DATE" data
 
-
-# $COMMON etl-scripts/historical_live_etl.py
-  
-# add more etl scripts here
-# $COMMON etl-scripts/more_scripts.py "$RELEASE_DATE" translink-data
