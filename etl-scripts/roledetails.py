8762c37d2aa5c44a90e72c8868619df86b17ea41
Viresh Soedhwa <vsoedhwa@bcit.ca>
2026-02-03 10:09:47 -0800

    add CSV output to roledetails.py and create notebook readme

--- etl-scripts/roledetails.py
+++ etl-scripts/roledetails.py
@@ -165,16 +165,20 @@ def main(raw_base: str, out_base: str):
 
     bad_rows = df.filter(F.col("RoleId").isNull() | F.col("RoleName").isNull())
     if bad_rows.count() > 0:
-        print("Found bad rows:")
-        bad_rows.select(
-        "OrgUnitId",  # or drop this if not needed
-        "RoleId",
-        "RoleName",
-        "RoleCode"
-        ).show(truncate=False)
-        sys.exit(1)
+        print(f"Found bad rows:{bad_rows.count()}")
+        # bad_rows.select(
+        # "OrgUnitId",  # or drop this if not needed
+        # "RoleId",
+        # "RoleName",
+        # "RoleCode"
+        # ).show(truncate=False)
+        # sys.exit(1)
+    
 
-
+    write_csv_publish(
+        format_ts_for_csv(df, ["LastModifiedDate"]),
+        "role_details",
+    )
 
     # df.printSchema()
 
