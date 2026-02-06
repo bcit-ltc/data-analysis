4076eeb811733ad9d28f149f9988811fbde03101
Arvin Rolos <arolos@bcit.ca>
2026-02-06 10:20:54 -0800

    Add releaseconditionsobjects ETL pipeline

--- etl.sh
+++ etl.sh
@@ -12,7 +12,8 @@ echo "[ETL] $(date -Iseconds) ETL scripts running ..."
 
 COMMON="/opt/spark/bin/spark-submit --master spark://spark-master:7077"
 
-$COMMON etl-scripts/audiovideoprocessed.py data output
+# $COMMON etl-scripts/audiovideoprocessed.py data output
 # $COMMON etl-scripts/contentservice.py data output
 # $COMMON etl-scripts/organizationalunits.py data output
+$COMMON etl-scripts/releaseconditionsobjects.py data output
 # $COMMON etl-scripts/roledetails.py data output
