f238f76a5183588cbaaf0cd42f92f6cadc95a7c5
Arvin Rolos <15988648+arolos@users.noreply.github.com>
2026-02-10 14:10:27 -0800

    Add discussionsforum (#28)

--- etl.sh
+++ etl.sh
@@ -13,9 +13,10 @@ echo "[ETL] $(date -Iseconds) ETL scripts running ..."
 COMMON="/opt/spark/bin/spark-submit --master spark://spark-master:7077"
 
 # $COMMON etl-scripts/audiovideoprocessed.py data output
+#$COMMON etl-scripts/contentobjects.py data output
 # $COMMON etl-scripts/contentservice.py data output
+$COMMON etl-scripts/discussionsforum.py data output
 # $COMMON etl-scripts/gradeobjects.py data output
 # $COMMON etl-scripts/organizationalunits.py data output
 # $COMMON etl-scripts/releaseconditionsobjects.py data output
-$COMMON etl-scripts/contentobjects.py data output
 # $COMMON etl-scripts/roledetails.py data output
